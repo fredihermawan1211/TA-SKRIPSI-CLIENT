cc7e73783218b8d748ca105a2efc8e22c7a5c995
Fredy Hermawan <fredihermawan1211@gmail.com>
2023-06-07 20:46:58 +0700

    Update Parameters

--- src/main/java/org/vaadin/views/Parameters.java
+++ src/main/java/org/vaadin/views/Parameters.java
@@ -5,9 +5,15 @@ public class Parameters {
     public Parameters() {
     }
     
+    // AWS 
+    // public static String PROTOCOL = "http://";
+    // public static String BASE_URL = "35.78.84.142";
+    // public static String PORT = ":9999";
+
+    // Local
     public static String PROTOCOL = "http://";
-    public static String BASE_URL = "35.78.88.189";
-    public static String PORT = ":9999";
+    public static String BASE_URL = "localhost";
+    public static String PORT = ":8080";
 
     public static String SERVER_URL = PROTOCOL+BASE_URL+PORT;
 }
